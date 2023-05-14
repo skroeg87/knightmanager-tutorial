0df8359e5d8068ff8c4be9fe9bb00bf7fecd14db
matgoor <92792437+matgoor@users.noreply.github.com>
2023-05-14 19:33:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
   <title>Knight Manager Tutorial</title>
   <link rel="stylesheet" type="text/css" href="styles.css">
+  <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
+  <![endif]-->
 </head>
 <body>
   <h1>Knight Manager Tutorial</h1>
